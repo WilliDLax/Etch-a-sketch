965284f6de54297d24ad4c6b92cf08de3879892b
William <shalomwill98@gmail.com>
2021-04-22 15:02:44 +0100

    Added color change when you hover over a cell
    added button to clear the grid and create a new one

--- etch.js
+++ etch.js
@@ -1,9 +1,48 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("button");
 
+//make 16x16 grid using css grid
 for (let i = 0; i < 16; i++){
     for(let j = 0; j < 16; j++){
         const cell = document.createElement("div");
         cell.classList.add("cell");
         container.appendChild(cell);
+
+        cell.addEventListener("mouseover", changeColor);
     }
 }
+
+//changes color when mouse hovers
+function changeColor(e){
+    this.classList.add("hovered");
+}
+
+//makes a new grid using the parameters as dimensions
+function makeGrid(size){
+    document.documentElement.style.setProperty("--size", size);
+    const newContainer = document.createElement("div");
+    newContainer.id = "container";
+    document.body.appendChild(newContainer);
+
+    for (let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            newContainer.appendChild(cell);
+    
+            cell.addEventListener("mouseover", changeColor);
+        }
+    }
+}
+
+//clears grid and asks for new dimensions
+button.onclick = function(){
+    let newSize = prompt("Choose a new size for the grid");
+
+    if(newSize > 65) alert("That's too much");
+    else if(isNaN(parseInt(newSize))) alert("Please use a number");
+    else{
+        document.querySelector("#container").remove();
+        makeGrid(newSize);
+    }
+};
